c49198b0b2a86a058e372412ef3aaad76f1670f7
vivekt20 <vivekcherupoika20@gmail.com>
2024-08-23 09:28:07 +0530

    programs on23/8/2024

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  MyApp({super.key});
+  @override
+  Widget build (BuildContext context){
+    return MaterialApp(
+    title:"Flutter App Demo",
+    debugShowCheckedModeBanner:false,
+    home:Scaffold(),
+   );
+  }
+}
+
+ 
+  
+          
